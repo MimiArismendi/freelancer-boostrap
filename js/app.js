6ec0203e40abb100d1e4c05873cdbd89a73ed795
noemi <noemiarismendi.cc@gmail.com>
2017-05-05 00:42:33 -0500

    funcional nav y modal

--- js/app.js
+++ js/app.js
@@ -1,3 +1,19 @@
 /*
  * Archivo principal de JS
  */
+ var modal = document.getElementById('fondoModal');
+ var img = document.getElementsByClassName('img');
+ var imgPortafolio = document.getElementById("img01");
+ var borrar = document.getElementsByClassName("cerrar")[0];
+ borrar.addEventListener("click", cerrar);
+
+ for(i=0; i< img.length; i++){
+   img[i].addEventListener("click", popUp);
+ }
+ function popUp(){
+     modal.style.display = "block";
+     imgPortafolio.src = this.src;
+ }
+ function cerrar() {
+     modal.style.display = "none";
+ }
